52fd1bf77388fcc16413d5a979534f23d78ca0e4
Prakash Gupta <prgupta@enacton.email>
2026-09-10 14:28:58 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <Button>Hello</Button>
      hello
     </div>
   );
